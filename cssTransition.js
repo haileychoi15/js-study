4c85d8fbf8a7a4dcdc4834e63fa91028d50a143f
yuyeong choi <imdud0612@gmail.com>
2020-07-17 18:10:41 +0900

    css transition

---
+++ cssTransition.js
@@ -0,0 +1,6 @@
+var change = document.querySelector(".change");
+change.style.transform = "scale(2)"; // transition 적용
+change.style.left = "400px"; // tansition 적용안함
+
+//var hover = document.querySelector(".hover-left");
+//hover.style.transform = "rotate(90deg)";
